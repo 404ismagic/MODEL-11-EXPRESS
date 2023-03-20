249fe2e7dbd7d555990477a6ea0b9cb2401086cf
magic404 <jamesbox3@gmail.com>
2023-03-20 11:16:18 -0700

    more some code

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5001;
 const { notes } = require("./db/db");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Unique ID
+const uniqueId = require('generate-unique-id');
