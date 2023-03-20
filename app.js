a65808246cbe6e9dff626f09fe0f08d39a3c54df
magic404 <jamesbox3@gmail.com>
2023-03-20 11:57:03 -0700

    FISNHED CODE BEFORE LAUNCH

--- app.js
+++ app.js
@@ -4,14 +4,15 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5001;
-const { notes } = require("./db/db");
-
+// const { notes } = require("./db/db.json");
+// console.log("notes = ", notes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // Unique ID
-const uniqueId = require('generate-unique-id');
+// const uniqueId = require('generate-unique-id');
+const { v4: uuidv4 } = require('uuid');
 
 //Create New Note
 function createANewNote(body, notesArray) {
@@ -19,7 +20,7 @@ function createANewNote(body, notesArray) {
     notesArray.push(note);
     fs.writeFileSync(
       path.join(__dirname, './db/db.json'),
-      JSON.stringify({ notes: notesArray }, null, 2)
+      JSON.stringify( notesArray, null, 2)
     );
     return note;
   };
@@ -34,12 +35,15 @@ app.get('/notes', (req, res) => {
 });
 
 app.get('/api/notes', (req, res) => {
-  res.json(notes);
+    
+  res.sendFile((path.join(__dirname, "./db/db.json")));
 });
 
 //Post Route
 app.post('/api/notes', (req, res) => {
-  req.body.id = uniqueId();
+  req.body.id = uuidv4();
+  let notes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  console.log("notes = ", notes);
   const note = createANewNote(req.body, notes);
   res.json(note);
 });
